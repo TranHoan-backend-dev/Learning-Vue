3f3c0024780b3c9f65f9c84b5f188198aacfd6f9
TranHoan2004 <hoana5k44nknd@gmail.com>
2026-05-07 22:33:47 +0700

    cập nhật sidebar

--- bài nghiên cứu/web/src/components/layout/ms-sidebar/Sidebar.vue
+++ bài nghiên cứu/web/src/components/layout/ms-sidebar/Sidebar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import type {SidebarItems} from "@/components/layout/ms-sidebar/model.ts";
 import CustomButton from "@/components/ui/ms-button/CustomButton.vue";
+import type { SidebarItems } from "./model";
 
 defineProps({
   isCollapsed: Boolean
@@ -14,47 +14,37 @@ const toggleSidebar = () => {
 const sidebarItems: SidebarItems[] = [
   {
     iconClassName: "sidebar_menu_item_recruitment_icon",
-    text: "Tin tuyển dụng",
-    to: "/hiring"
+    text: "Tổng quan",
+    to: "/overview"
   },
   {
     iconClassName: "sidebar_menu_item_user_icon",
-    text: "Ứng viên",
-    to: "/candidates",
+    text: "Thành phần lương",
+    to: "/components",
     parentClassName: "sidebar_menu_item_active"
   },
   {
     iconClassName: "sidebar_menu_item_calendar_icon",
-    text: "Lịch",
-    to: "/schedule"
+    text: "Mẫu bảng lương",
+    to: "/sample"
   },
   {
     iconClassName: "sidebar_menu_item_tiem_nang_icon",
-    text: "Kho tiềm năng",
-    to: "/talent"
+    text: "Dữ liệu tính lương",
+    to: "/data"
   },
   {
     iconClassName: "sidebar_menu_item_hiring_campaign_icon",
-    text: "Chiến dịch tuyển dụng",
-    to: "/media"
+    text: "Tính lương",
+    to: "/calculate"
   },
   {
     iconClassName: "sidebar_menu_item_job_icon",
-    text: "Công việc",
-    to: "/job"
+    text: "Chi trả",
+    to: "/pay"
   },
   {
     iconClassName: "sidebar_menu_item_ai_marketing_icon",
-    text: "aiMarketing",
-    to: "/marketing"
-  },
-  {
-    iconClassName: "sidebar_menu_item_contact_icon",
-    text: "Trao đổi với ứng viên",
-    to: "/conversation"
-  },
-  {
-    iconClassName: "sidebar_menu_item_report_icon",
     text: "Báo cáo",
     to: "/report"
   },
@@ -62,57 +52,32 @@ const sidebarItems: SidebarItems[] = [
     iconClassName: "sidebar_menu_item_setting_icon",
     text: "Thiết lập",
     to: "/setting"
-  },
-  {
-    iconClassName: "sidebar_menu_item_knowledge_icon",
-    text: "Kiến thức hữu ích",
-    to: "/knowledge"
-  },
+  }
 ];
 </script>
 
 <template>
-  <div class="sidebar" :class="{'sidebar_collapsed': isCollapsed}">
+  <div class="sidebar" :class="{ 'sidebar_collapsed': isCollapsed }">
     <div class="sidebar_bg">
       <div class="sidebar_menu">
-        <CustomButton
-            v-for="(component, index) in sidebarItems"
-            class="sidebar_menu_item"
-            :key="index"
-            :to="component.to"
-            variant="text"
-            :width="isCollapsed ? 36 : 209"
-            :height="40"
-            :is-loading="false"
-            :style="{
-              marginLeft: isCollapsed ? '5px' : '12px',
-              justifyContent: isCollapsed ? 'center' : 'left'
-            }"
-            tooltip-location="right"
-            :tooltip-content="component.text"
-        >
+        <CustomButton v-for="(component, index) in sidebarItems" class="sidebar_menu_item" :key="index"
+          :to="component.to" variant="text" :width="isCollapsed ? 36 : 209" :height="40" :is-loading="false" :style="{
+            marginLeft: isCollapsed ? '5px' : '12px',
+            justifyContent: isCollapsed ? 'center' : 'left'
+          }" tooltip-location="right" :tooltip-content="component.text">
           <div :class="component.iconClassName"></div>
           <div class="sidebar_menu_item_text">{{ component.text }}</div>
         </CustomButton>
       </div>
 
-      <CustomButton
-          class-name="sidebar_toggle"
-          :label="isCollapsed ? '' : 'Thu gọn'"
-          :prepend-icon="isCollapsed ? 'mdi-chevron-double-right' : 'mdi-chevron-double-left'"
-          @click="toggleSidebar"
-          min-width="35"
-          height="40"
-          :width="isCollapsed ? 0 : 209"
-          :is-loading="false"
-          variant="outlined"
-          style="color: white; border-color: #dee2e6;"
-          tooltip-location="right"
-          tooltip-content="Thu gọn"
-      />
+      <CustomButton class-name="sidebar_toggle" @click="toggleSidebar"
+        :width="isCollapsed ? 36 : 209" :height="44" :is-loading="false" variant="text"
+        tooltip-location="right" tooltip-content="Thu gọn">
+        <div class="sidebar_toggle_icon" :style="{ transform: isCollapsed ? 'rotate(180deg)' : 'none' }"></div>
+        <div v-if="!isCollapsed" class="sidebar_toggle_text">Thu gọn</div>
+      </CustomButton>
     </div>
   </div>
 </template>
 
-<style scoped src="./style.css">
-</style>
+<style scoped src="./style.css"></style>
